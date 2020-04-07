42bafe3088ecbc6f8c6bd950689a4bd7f4390e22
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2020-04-08 00:39:42 +0700

    Add user_type field and name field for the User model and change view devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  # Validates
+  validates :name, presence: true
+  validates :user_type, presence: true, inclusion: { in: %w(Candidate Employer Admin) }
+
+  USER_TYPE = %w{ Candidate Employer }
 end
