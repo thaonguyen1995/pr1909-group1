bb9b77289957af0e4c5b38fbf1d60a21bc0fa797
Thao <nguyenvanthaobg1995@gmail.com>
2020-04-08 20:57:52 +0700

    redirect to candidate and employer after login

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,8 @@
+class CandidatesController < ApplicationController
+  def show
+    @user = User.find_by id: params[:id]
+  end
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :employers
+  resources :candidates
   root 'blog#index'
-  devise_for :users
+  devise_for :users, :controllers => {:sessions => "sessions"}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
