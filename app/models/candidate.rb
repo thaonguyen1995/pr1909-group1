6a60d0ff85020d1e662fc6e7b3e9d63cc683da88
Thao <nguyenvanthaobg1995@gmail.com>
2020-04-10 09:46:52 +0700

    update ENV

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,5 +1,5 @@
 class Candidate < ApplicationRecord
   has_one :user
   has_many :apply_activities
-  validates :candidate_name, :user_id, presence:true
+  validates :candidate_name, :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
-  enum user_type: {:candidates => "candidates", :employers => "employers"}
+  enum user_type: { :candidates => "candidates", :employers => "employers" }
 
   # Validates
   validates :name, presence: true
