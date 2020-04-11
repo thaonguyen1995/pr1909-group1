4d2a47dc38c1e4667bda15e48a4bde9692fd95db
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2020-04-12 01:26:38 +0700

    create user in file seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,19 @@ User.create!(
   user_type: "admin",
   confirmed_at: Time.zone.now
 )
+User.create!(
+  name: "a", 
+  email: "a@gmail.com",
+  password:  "123456",
+  password_confirmation: "123456", 
+  user_type: "candidate",
+  confirmed_at: Time.zone.now
+)
+User.create!(
+  name: "b", 
+  email: "b@gmail.com",
+  password:  "123456",
+  password_confirmation: "123456", 
+  user_type: "employer",
+  confirmed_at: Time.zone.now
+)
