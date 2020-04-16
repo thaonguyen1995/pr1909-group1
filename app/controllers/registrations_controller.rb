ecb60b9a3402ed3ce76bbf70c3bb7537b8856df8
Thao <nguyenvanthaobg1995@gmail.com>
2020-04-16 21:29:57 +0700

    fix as requirement 2

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,11 +8,15 @@ class RegistrationsController < Devise::RegistrationsController
     user = User.find_by email: params[:user][:email]
     return unless user
 <<<<<<< HEAD
+<<<<<<< HEAD
     object = user.user_type.classify.constantize.new user_id: user.id
 =======
     current_resource = user.user_type.camelize.constantize
     object = current_resource.new user_id: user.id
 >>>>>>> fix as requirement 1
+=======
+    object = user.user_type.classify.constantize.new user_id: user.id
+>>>>>>> fix as requirement 2
     object.save
   end
 end
