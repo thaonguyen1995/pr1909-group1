8803f7a132c1b76a8876e78a0f5ba828acab52fc
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2020-04-20 00:20:12 +0700

    fix some convention and delete file test upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :job_posts
-  mount Ckeditor::Engine => '/ckeditor'
   root 'blog#index'
 
   resources :employers
   resources :candidates
+  resources :job_posts
   devise_for :users, :controllers => { registrations: "registrations" }
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount Ckeditor::Engine => '/ckeditor'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
