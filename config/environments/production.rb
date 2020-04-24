9f55705d5c38e646fbd872fc03c08b4f119c5845
tuanhv-0694 <49422593+tuanhv-0694@users.noreply.github.com>
2020-04-25 04:29:07 +0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { host: 'https://infinite-cove-52611.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: ENV["USER_EMAIL"],
+    password: ENV["USER_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+  }
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
